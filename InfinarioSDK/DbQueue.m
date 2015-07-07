b08799a406e6963dc99a42021f58abd791d3de6c
Roland Rogansky <roland.rogansky@infinario.com>
2015-07-07 13:48:46 +0200

    Make SDK safe thread
    Updated DbQueue.m to thread safe

--- InfinarioSDK/DbQueue.m
+++ InfinarioSDK/DbQueue.m
@@ -14,6 +14,7 @@ static int const POP_LIMIT = 50;
 @interface DbQueue ()
 
 @property DbManager *dbManager;
+@property NSObject *lockAccess;
 
 @end
 
@@ -22,6 +23,7 @@ static int const POP_LIMIT = 50;
 - (instancetype) init {
     self = [super init];
     
+    self.lockAccess = [[NSObject alloc] init];
     self.dbManager = [[DbManager alloc] initWithDatabaseFilename: DB_FILE];
     [self createDbIfNecessary];
     
@@ -29,55 +31,65 @@ static int const POP_LIMIT = 50;
 }
 
 - (void) schedule:(NSDictionary *)command {
-    NSData *data = [NSJSONSerialization dataWithJSONObject:command options:0 error:nil];
-    NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    [self.dbManager executeQuery:[NSString stringWithFormat: @"INSERT INTO commands (command) VALUES ('%@');", str]];
+    @synchronized(self.lockAccess){
+        NSData *data = [NSJSONSerialization dataWithJSONObject:command options:0 error:nil];
+        NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        [self.dbManager executeQuery:[NSString stringWithFormat: @"INSERT INTO commands (command) VALUES ('%@');", str]];
+    }
 }
 
 - (NSArray *) pop:(int) limit {
-    NSMutableArray *requests = [[NSMutableArray alloc] init];
-    NSMutableDictionary *request;
-    NSMutableDictionary *commandData;
-    NSData *data;
+    @synchronized(self.lockAccess){
+        NSMutableArray *requests = [[NSMutableArray alloc] init];
+        NSMutableDictionary *request;
+        NSMutableDictionary *commandData;
+        NSData *data;
     
-    NSArray *commands = [self.dbManager loadDataFromDb:[NSString stringWithFormat:@"SELECT * FROM commands LIMIT %d;", limit]];
+        NSArray *commands = [self.dbManager loadDataFromDb:[NSString stringWithFormat:@"SELECT * FROM commands LIMIT %d;", limit]];
 
-    for (NSMutableArray *command in commands) {
-        data = [command[1] dataUsingEncoding:NSUTF8StringEncoding];
-        commandData =[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+        for (NSMutableArray *command in commands) {
+            data = [command[1] dataUsingEncoding:NSUTF8StringEncoding];
+            commandData =[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
         
-        request = [[NSMutableDictionary alloc] init];
+            request = [[NSMutableDictionary alloc] init];
         
-        request[@"id"] = command[0];
-        request[@"command"] = commandData;
+            request[@"id"] = command[0];
+            request[@"command"] = commandData;
         
-        [requests addObject:request];
-    }
+            [requests addObject:request];
+        }
     
-    return requests;
+        return requests;
+    }
 }
 
 - (NSArray *) pop {
-    return [self pop:POP_LIMIT];
+        return [self pop:POP_LIMIT];
 }
 
 - (BOOL) isEmpty {
-    NSArray *result = [self.dbManager loadDataFromDb:@"SELECT COUNT(*) FROM commands;"];
+    @synchronized(self.lockAccess){
+        NSArray *result = [self.dbManager loadDataFromDb:@"SELECT COUNT(*) FROM commands;"];
 
-    return [result[0][0] isEqualToString:@"0"];
+        return [result[0][0] isEqualToString:@"0"];
+    }
 }
 
 - (void) clear:(NSArray *)successfull andFailed:(NSArray *)failed {
-    [self.dbManager executeQuery:[NSString stringWithFormat:@"DELETE FROM commands WHERE id IN (%@)", [successfull componentsJoinedByString:@","]]];
-    [self.dbManager executeQuery:[NSString stringWithFormat:@"UPDATE commands SET retries = retries + 1 WHERE id IN (%@);", [failed componentsJoinedByString:@","]]];
-    [self.dbManager executeQuery:[NSString stringWithFormat:@"DELETE FROM commands WHERE retries > %d", MAX_RETRIES]];
+    @synchronized(self.lockAccess){
+        [self.dbManager executeQuery:[NSString stringWithFormat:@"DELETE FROM commands WHERE id IN (%@)", [successfull componentsJoinedByString:@","]]];
+        [self.dbManager executeQuery:[NSString stringWithFormat:@"UPDATE commands SET retries = retries + 1 WHERE id IN (%@);", [failed componentsJoinedByString:@","]]];
+        [self.dbManager executeQuery:[NSString stringWithFormat:@"DELETE FROM commands WHERE retries > %d", MAX_RETRIES]];
+    }
 }
 
 - (void) createDbIfNecessary {
-    [self.dbManager executeQuery:@"CREATE TABLE IF NOT EXISTS commands ("
+    @synchronized(self.lockAccess){
+        [self.dbManager executeQuery:@"CREATE TABLE IF NOT EXISTS commands ("
                                     " id INTEGER PRIMARY KEY AUTOINCREMENT,"
                                     " command TEXT NOT NULL,"
                                     " retries INTEGER NOT NULL DEFAULT 0);"];
+    }
 }
 
 @end
