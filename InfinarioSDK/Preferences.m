bf483396f5b0b6921cb3f0189df21d99951574c2
Roland Rogansky <roland.rogansky@infinario.com>
2015-09-02 14:05:42 +0200

    Make preferences safe thread

--- InfinarioSDK/Preferences.m
+++ InfinarioSDK/Preferences.m
@@ -12,6 +12,7 @@
 
 @property DbManager *dbManager;
 @property NSMutableDictionary *prefs;
+@property NSObject *lockAccess;
 
 @end
 
@@ -20,6 +21,7 @@
 - (instancetype)init {
     self = [super init];
     
+    self.lockAccess = [[NSObject alloc] init];
     self.dbManager = [[DbManager alloc] initWithDatabaseFilename: DB_FILE];
     [self createDbIfNecessary];
     
@@ -41,22 +43,26 @@
 }
 
 - (id)objectForKey:(NSString *)key {
-    return self.prefs[key];
+    @synchronized(self.lockAccess){
+        return self.prefs[key];
+    }
 }
 
 - (id)objectForKey:(NSString *)key withDefault:(id)defaultValue {
-    id value = [self objectForKey:key];
+    @synchronized(self.lockAccess){
+        id value = [self objectForKey:key];
     
-    if (!value) {
-        return defaultValue;
+        if (!value) {
+            return defaultValue;
+        }
+    
+        return value;
     }
-    
-    return value;
 }
 
 - (void)setObject:(id)value forKey:(NSString *)key {
     if (value && key) {
-        @synchronized(self) {
+        @synchronized(self.lockAccess) {
             [self.prefs setValue:value forKey:key];
             [self save:self.prefs];
         }
@@ -64,35 +70,43 @@
 }
 
 - (void)removeObjectForKey:(NSString *)key {
-    if (key) {
-        [self.prefs removeObjectForKey:key];
-        [self save:self.prefs];
+    @synchronized(self.lockAccess){
+        if (key) {
+            [self.prefs removeObjectForKey:key];
+            [self save:self.prefs];
+        }
     }
 }
 
 - (NSMutableDictionary *)load {
-    NSArray *prefsArray = [self.dbManager loadDataFromDb:@"SELECT * FROM preferences;"];
-    NSMutableDictionary *prefs = [[NSMutableDictionary alloc] init];
+    @synchronized(self.lockAccess){
+        NSArray *prefsArray = [self.dbManager loadDataFromDb:@"SELECT * FROM preferences;"];
+        NSMutableDictionary *prefs = [[NSMutableDictionary alloc] init];
     
-    for (NSArray *option in prefsArray) {
-        prefs[option[0]] = option[1];
+        for (NSArray *option in prefsArray) {
+            prefs[option[0]] = option[1];
+        }
+    
+        return prefs;
     }
-    
-    return prefs;
 }
 
 - (void)save:(NSDictionary *)preferences {
-    [self.dbManager executeQuery:@"DELETE FROM preferences;"];
+    @synchronized(self.lockAccess){
+        [self.dbManager executeQuery:@"DELETE FROM preferences;"];
     
-    for (NSString *key in preferences) {
-        [self.dbManager executeQuery:[NSString stringWithFormat:@"INSERT INTO preferences (key, value) VALUES ('%@', '%@')", key, preferences[key]]];
+        for (NSString *key in preferences) {
+            [self.dbManager executeQuery:[NSString stringWithFormat:@"INSERT INTO preferences (key, value) VALUES ('%@', '%@')", key, preferences[key]]];
+        }
     }
 }
 
 - (void)createDbIfNecessary {
-    [self.dbManager executeQuery:@"CREATE TABLE IF NOT EXISTS preferences ("
+    @synchronized(self.lockAccess){
+        [self.dbManager executeQuery:@"CREATE TABLE IF NOT EXISTS preferences ("
                                  " key TEXT PRIMARY KEY NOT NULL,"
                                  " value TEXT NOT NULL);"];
+    }
 }
 
 @end
